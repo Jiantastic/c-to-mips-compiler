a66b4127f631789dacbdc4cea3f2ca2ef109033b
Jian Wong <srswwj@gmail.com>
2016-02-06 17:42:43 +0000

    ~finished lab4, initial framework for lexers done

---
+++ Compiler/c-to-mips/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	int xmen = 3ul;
+	return 0;
+}
---
+++ Compiler/lexer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	string x;
+	while(cin >> x){
+		cout << x << endl;
+	}
+	cout << "end" << endl;
+	return 0;
+}
